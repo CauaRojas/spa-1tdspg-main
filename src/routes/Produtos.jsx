a28834bc05d7756a354ad2558be3c6e68b24df6d
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-10-05 18:27:57 -0300

    added alter

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,114 +1,138 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 // import { ListaProdutos } from "../components/ListaProdutos";
 import {
-  AiOutlineEdit as Editar,
-  AiOutlineDelete as Excluir,
-} from "react-icons/ai";
-import classes from "./Produtos.module.css";
-import { useEffect, useState } from "react";
-import ModalExemplo from "../components/ModalExemplo/ModalExemplo";
+    AiOutlineEdit as Editar,
+    AiOutlineDelete as Excluir,
+} from 'react-icons/ai'
+import classes from './Produtos.module.css'
+import { useEffect, useState } from 'react'
+import ModalExemplo from '../components/ModalExemplo/ModalExemplo'
 
 export default function Produtos() {
-  document.title = "Produtos";
+    document.title = 'Produtos'
 
-  const [novaListaProdutos, setNovaListaProdutos] = useState([{}]);
+    const [novaListaProdutos, setNovaListaProdutos] = useState([{}])
 
-  // useEffect(() => {
-  //   setNovaListaProdutos(ListaProdutos);
-  //   console.log("useEffect que renderiza apenas uma vez!");
-  // }, []);
+    // useEffect(() => {
+    //   setNovaListaProdutos(ListaProdutos);
+    //   console.log("useEffect que renderiza apenas uma vez!");
+    // }, []);
 
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false)
+    const [editProduto, setEditProduto] = useState({})
 
-  useEffect(() => {
-    if (!open) {
-      //fetch = API do Javascript para realizar requisições/requests, ele utiliza como parâmetro uma URL ou URI.
-      //fetch(http://minhaApi.com.br/exemplos)
-      fetch("http://localhost:5000/produtos", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setNovaListaProdutos(data);
+    useEffect(() => {
+        if (!open) {
+            //fetch = API do Javascript para realizar requisições/requests, ele utiliza como parâmetro uma URL ou URI.
+            //fetch(http://minhaApi.com.br/exemplos)
+            fetch('http://localhost:5000/produtos', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then(response => response.json())
+                .then(data => {
+                    setNovaListaProdutos(data)
+                })
+                .catch(error => console.log(error))
+        }
+        setEditProduto({})
+    }, [open])
+
+    const handleExcluir = id => {
+        fetch(`http://localhost:5000/produtos/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
         })
-        .catch((error) => console.log(error));
+            .then(response => console.log(response.status))
+            .catch(error => console.log(error))
+
+        window.location('/produtos')
     }
-  }, [open]);
 
-  const handleExcluir = (id) => {
-    fetch(`http://localhost:5000/produtos/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => console.log(response.status))
-      .catch((error) => console.log(error));
+    return (
+        <>
+            <div>
+                <h1>PRODUTOS</h1>
 
-    window.location("/produtos");
-  };
+                {open ? (
+                    <ModalExemplo
+                        open={open}
+                        setOpen={setOpen}
+                        produto={editProduto}
+                    />
+                ) : (
+                    ''
+                )}
 
-  return (
-    <>
-      <div>
-        <h1>PRODUTOS</h1>
+                <Link onClick={() => setOpen(true)}>Adicionar Produto</Link>
 
-        {open ? <ModalExemplo open={open} setOpen={setOpen} /> : ""}
-
-        <Link onClick={() => setOpen(true)}>Adicionar Produto</Link>
-
-        <table className={classes.tabelaProd}>
-          <thead className={classes.tabelaCabecalho}>
-            <tr>
-              <th>ID</th>
-              <th>NOME</th>
-              <th>DESCRIÇÃO</th>
-              <th>PREÇO</th>
-              <th>IMG</th>
-              <th>
-                <Editar /> / <Excluir />
-              </th>
-            </tr>
-          </thead>
-          <tbody className={classes.tabelaCorpo}>
-            {novaListaProdutos.map((produto, indice) => (
-              <tr key={indice}>
-                <td>{produto.id}</td>
-                <td>{produto.nome}</td>
-                <td>{produto.desc}</td>
-                <td>{produto.preco}</td>
-                <td>
-                  <img
-                    className={classes.tblImg}
-                    src={`${produto.img}`}
-                    alt={`${produto.desc}`}
-                  />
-                </td>
-                <td>
-                  {" "}
-                  <Link to={`/editar/produto/${produto.id}`}>
-                    <Editar />
-                  </Link>{" "}
-                  /{" "}
-                  <Link onClick={() => handleExcluir(produto.id)}>
-                    <Excluir />
-                  </Link>{" "}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-          <tfoot className={classes.tabelaRodape}>
-            <tr>
-              <td colSpan={6}>
-                PRODUTOS INFORMÁTICOS - QTD = {novaListaProdutos.length}
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
-    </>
-  );
+                <table className={classes.tabelaProd}>
+                    <thead className={classes.tabelaCabecalho}>
+                        <tr>
+                            <th>ID</th>
+                            <th>NOME</th>
+                            <th>DESCRIÇÃO</th>
+                            <th>PREÇO</th>
+                            <th>IMG</th>
+                            <th>
+                                <Editar /> / <Excluir /> / A
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody className={classes.tabelaCorpo}>
+                        {novaListaProdutos.map((produto, indice) => (
+                            <tr key={indice}>
+                                <td>{produto.id}</td>
+                                <td>{produto.nome}</td>
+                                <td>{produto.desc}</td>
+                                <td>{produto.preco}</td>
+                                <td>
+                                    <img
+                                        className={classes.tblImg}
+                                        src={`${produto.img}`}
+                                        alt={`${produto.desc}`}
+                                    />
+                                </td>
+                                <td>
+                                    {' '}
+                                    <Link to={`/editar/produto/${produto.id}`}>
+                                        <Editar />
+                                    </Link>{' '}
+                                    /{' '}
+                                    <Link
+                                        onClick={() =>
+                                            handleExcluir(produto.id)
+                                        }
+                                    >
+                                        <Excluir />
+                                    </Link>{' '}
+                                    /{' '}
+                                    <Link
+                                        onClick={() => {
+                                            setEditProduto(produto)
+                                            setOpen(true)
+                                        }}
+                                    >
+                                        A
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    <tfoot className={classes.tabelaRodape}>
+                        <tr>
+                            <td colSpan={6}>
+                                PRODUTOS INFORMÁTICOS - QTD ={' '}
+                                {novaListaProdutos.length}
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </>
+    )
 }
